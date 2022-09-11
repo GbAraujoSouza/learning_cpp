f799252d0b4154fcebb25e160a4b2c065e93c179
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2022-09-11 20:40:39 -0300

    inheritabce

--- oop.cpp
+++ oop.cpp
@@ -3,65 +3,103 @@
 using namespace std;
 
 class YouTubeChannel {
-    private:
-        string Name;
-        string OwnerName;
-        int SubscribersCount;
-        list<string> PublishedVideoTitles;
+private:
+    int SubscribersCount;
+    list<string> PublishedVideoTitles;
+    
+protected:
+    string Name;
+    string OwnerName;
 
-    public:
-        YouTubeChannel(string name, string ownerName) {
-            Name = name;
-            OwnerName = ownerName;
-            SubscribersCount = 0;
-        }
+public:
+    YouTubeChannel(string name, string ownerName) {
+        Name = name;
+        OwnerName = ownerName;
+        SubscribersCount = 0;
+    }
 
-        void GetInfo() {
-            cout << "Name: " << Name << endl;
-            cout << "Owner: " << OwnerName << endl;
-            cout << "Subscribers: " << SubscribersCount << endl;
-            cout << "Videos: "  << endl;
+    void GetInfo() {
+        cout << "Name: " << Name << endl;
+        cout << "Owner: " << OwnerName << endl;
+        cout << "Subscribers: " << SubscribersCount << endl;
+        cout << "Videos: "  << endl;
 
-            // NEW THING!
-            for (string i : PublishedVideoTitles){
-                cout << "> " << i << endl;
-            }
+        // NEW THING!
+        for (string i : PublishedVideoTitles){
+            cout << "> " << i << endl;
         }
+    }
 
-        void Subscribe() {
-            SubscribersCount++;
-        }
+    void Subscribe() {
+        SubscribersCount++;
+    }
 
-        void Unsubscribe() {
-            if (SubscribersCount > 0)
-                SubscribersCount--;
-        }
+    void Unsubscribe() {
+        if (SubscribersCount > 0)
+            SubscribersCount--;
+    }
 
-        void PublishVideo(string title) {
-            PublishedVideoTitles.push_back(title);
-        }
+    void PublishVideo(string title) {
+        PublishedVideoTitles.push_back(title);
+    }
 
-        string GetName() {
-            return Name;
-        }
-        
-        void SetName(string new_name) {
-            Name = new_name;  
-        }
+    // Getters
+    string GetName() {
+        return Name;
+    }
+    string GetOwnerName() {
+        return OwnerName;
+    }
+    int GetSubscribersCount() {
+        return SubscribersCount;
+    }
+    list<string> GetPublishedVideosTitles() {
+        return PublishedVideoTitles;
+    }
+
+    void SetName(string new_name) {
+        Name = new_name;  
+    }
 };
+
+
+class CookingYouTubeChannel:public YouTubeChannel
+{
+private:
+    int CookingLevel;
+public:
+    CookingYouTubeChannel(string name, string ownerName):YouTubeChannel(name, ownerName) {
+        CookingLevel = 0;
+    }
+    void Practice() {
+        cout << Name << " is practicing cooking, learning new recipes, experimenting with spices...\n";
+        CookingLevel++;
+    }
+    void GetInfo() {
+        cout << "Name: " << Name << endl;
+        cout << "Owner: " << OwnerName << endl;
+        cout << "Subscribers: " << GetSubscribersCount() << endl;
+        cout << "Videos: "  << endl;
+        // NEW THING!
+        for (string i : GetPublishedVideosTitles()){
+            cout << "> " << i << endl;
+        }
+        cout << "Cooking Level: " << CookingLevel << endl;
+    }
+};
+
 
 int main()
 {
-    YouTubeChannel ytChannel("CodeBeauty", "Saldina");
-    ytChannel.PublishVideo("C++ For Beginners");
-    ytChannel.PublishVideo("C++ OOP Tutorial");
-    YouTubeChannel ytChannel2("AmySings", "Amy");
+    CookingYouTubeChannel cookingYtChannel("Amy Cooks", "Amy");
 
-    // Write out the object attributes
-    ytChannel.Unsubscribe();
-    cout << ytChannel.GetName() << endl;
-    ytChannel.SetName("TrueCodeBeauty");
-    cout << ytChannel.GetName() << endl;
+    cookingYtChannel.PublishVideo("Rice With Beans");
+    cookingYtChannel.PublishVideo("Carbonara");
+    for (int user = 0; user < 100; user++) {
+        cookingYtChannel.Subscribe();
+    }
+    cookingYtChannel.Practice();
+    cookingYtChannel.GetInfo();
 
 
     return 0;
